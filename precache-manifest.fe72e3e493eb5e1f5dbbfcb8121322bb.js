1c3965e618bad84db80e94ac6f916d2b8325a730
Maarten de Rijk <maarten.derijk@gmail.com>
2019-12-14 19:31:09 +0100

    Updates

--- precache-manifest.fe72e3e493eb5e1f5dbbfcb8121322bb.js
+++ precache-manifest.fe72e3e493eb5e1f5dbbfcb8121322bb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ecbc1d0e27e2c2752db70381e872a10",
+    "revision": "e6ec95cbde2d1653c3a61874ddf30fd7",
     "url": "/homepage_react/index.html"
   },
   {
-    "revision": "4d8e4c3dd179d2a37513",
+    "revision": "fad92ccf7c7d23a26db2",
     "url": "/homepage_react/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "40fa810e257bc80ec93e",
-    "url": "/homepage_react/static/js/2.df6742d5.chunk.js"
+    "revision": "ab607c7bf9489b180113",
+    "url": "/homepage_react/static/js/2.45be32ee.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/homepage_react/static/js/2.df6742d5.chunk.js.LICENSE"
+    "url": "/homepage_react/static/js/2.45be32ee.chunk.js.LICENSE"
   },
   {
-    "revision": "4d8e4c3dd179d2a37513",
-    "url": "/homepage_react/static/js/main.0c6b167e.chunk.js"
+    "revision": "fad92ccf7c7d23a26db2",
+    "url": "/homepage_react/static/js/main.999f5ac6.chunk.js"
   },
   {
     "revision": "f66bb8a0239b2513f1cc",
