040fed02b7eb06be362c5554ada9d7648ad49508
Maarten de Rijk <maarten.derijk@gmail.com>
2019-12-14 16:20:23 +0100

    Updates

---
+++ precache-manifest.7d5b249ea162dfafdf50bc52b1b03458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f4c1004146c9650fd89991e47607c0",
+    "url": "/homepage_react/index.html"
+  },
+  {
+    "revision": "9a5ce18a081eaedbb70e",
+    "url": "/homepage_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40fa810e257bc80ec93e",
+    "url": "/homepage_react/static/js/2.df6742d5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/homepage_react/static/js/2.df6742d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a5ce18a081eaedbb70e",
+    "url": "/homepage_react/static/js/main.cea5fe66.chunk.js"
+  },
+  {
+    "revision": "f66bb8a0239b2513f1cc",
+    "url": "/homepage_react/static/js/runtime-main.1471f7dc.js"
+  }
+]);
